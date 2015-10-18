4af74da6de81609dc5fc680ac28eab832bcb3dac
Harrison Reighard <hpreighard@gmail.com>
2015-10-18 03:26:17 -0500

    Update client.rb
    Formatting and organizing for actual stuff later

--- client.rb
+++ client.rb
@@ -1,1 +1,15 @@
+require 'socket'
+puts "Client Side File Sharing"
+puts "Choose an option"
+streamSock = TCPSocket.new("", 8686)
 
+loop do
+  #send request to create tracker
+  
+  #send request to update tracker
+  
+  #request list of all trackers
+  
+  #request server to send file
+
+end
