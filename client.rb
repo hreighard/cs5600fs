c0fdc456ae99dbc1b00565eec0c72445bffc49ef
Harrison Reighard <hpreighard@gmail.com>
2015-10-18 11:16:12 -0500

    Update client.rb
    Send commands to server

--- client.rb
+++ client.rb
@@ -1,15 +1,15 @@
 require 'socket'
 puts "Client Side File Sharing"
 puts "Choose an option"
-streamSock = TCPSocket.new("", 8686)
+s = TCPSocket.new("", 8686)
 
 loop do
   #send request to create tracker
-  
+    s.send "<createtracker filename filesize description md5 ipaddres portnumber>"
   #send request to update tracker
-  
+    s.send "<updatetracker filename startbytes endbytes>"
   #request list of all trackers
-  
+    s.send "<REQ list>"
   #request server to send file
-
+    s.send "<GET filename>
 end
